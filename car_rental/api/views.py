b2255a2e1bbb5cbb1fd4c583469a8a0088746ae3
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-10-07 11:54:38 -0600

    Add employee view to URL

--- car_rental/api/views.py
+++ car_rental/api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
-from .serializers import BranchSerializer
-from .models import Branch
+from .serializers import BranchSerializer, EmployeeSerializer
+from .models import Branch, Employee
 
 # Create your views here.
 
 class BranchView(viewsets.ModelViewSet):
     serializer_class = BranchSerializer
     queryset = Branch.objects.all()
+
+class EmployeeView(viewsets.ModelViewSet):
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
