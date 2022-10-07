d9c86ec9e8b6d097340d962328f27838fc3c6c2d
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-10-07 14:42:35 -0600

    Add employee phone number view

--- car_rental/api/views.py
+++ car_rental/api/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
-from .serializers import BranchSerializer, EmployeeSerializer, CustomerSerializer
-from .models import Branch, Employee, Customer
+from .serializers import BranchSerializer, EmployeePhoneNumberSerializer, EmployeeSerializer, CustomerSerializer
+from .models import Branch, Employee, Customer, EmployeePhoneNumber
 
 # Create your views here.
+
 
 class BranchView(viewsets.ModelViewSet):
     serializer_class = BranchSerializer
     queryset = Branch.objects.all()
 
+
 class EmployeeView(viewsets.ModelViewSet):
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
+
+
+class EmployeePhoneNumberView(viewsets.ModelViewSet):
+    serializer_class = EmployeePhoneNumberSerializer
+    queryset = EmployeePhoneNumber.objects.all()
+
 
 class CustomerView(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
