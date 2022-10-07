ce967dbd6a68c882a2a4ed755c26ca86a3e328d0
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-10-07 12:09:15 -0600

    Change DOB to DateField instead of DateTimeField

--- car_rental/api/models.py
+++ car_rental/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Branch(models.Model):
     id = models.AutoField(primary_key=True)
     province = models.CharField(max_length=30)
@@ -9,6 +11,7 @@ class Branch(models.Model):
     streetnumber = models.CharField(max_length=10)
     streetname = models.CharField(max_length=30)
     unitnumber = models.CharField(max_length=5)
+
 
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
@@ -20,11 +23,10 @@ class Employee(models.Model):
     salt = models.CharField(max_length=255)
     salary = models.IntegerField()
     rank = models.CharField(max_length=30)
-    DOB = models.DateTimeField()
+    DOB = models.DateField()
     province = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
     postal_code = models.CharField(max_length=6)
     street_number = models.CharField(max_length=10)
     street_name = models.CharField(max_length=30)
     unit_number = models.CharField(max_length=5)
-    
