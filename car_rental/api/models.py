586289c6408aa1bd131e956b4f78e04de5376859
josephlatina <josephlatina93@gmail.com>
2022-10-07 14:21:38 -0600

    changed Branch table fields into snake case format

--- car_rental/api/models.py
+++ car_rental/api/models.py
@@ -7,10 +7,10 @@ class Branch(models.Model):
     id = models.AutoField(primary_key=True)
     province = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
-    postalcode = models.CharField(max_length=6)
-    streetnumber = models.CharField(max_length=10)
-    streetname = models.CharField(max_length=30)
-    unitnumber = models.CharField(max_length=5)
+    postal_code = models.CharField(max_length=6)
+    street_number = models.CharField(max_length=10)
+    street_name = models.CharField(max_length=30)
+    unit_number = models.CharField(max_length=5)
 
 
 class Employee(models.Model):
