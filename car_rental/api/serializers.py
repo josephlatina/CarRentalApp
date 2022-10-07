bf266fad80b83f63e57bc3120f4d2c5de9a51d22
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-10-07 11:48:45 -0600

    Add serializer for employee

--- car_rental/api/serializers.py
+++ car_rental/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Branch
+from .models import Branch, Employee
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branch
         fields = ('id', 'province', 'city', 'postalcode', 'streetnumber', 'streetname', 'unitnumber')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('id', 'first_name', 'last_name', 'email', 'password', 'salt', 'rank', 'DOB', 'province', 'city', 'postal_code', 'street_number', 'unit_number')
