f336e169e3bc826a935e3afe578ee40de73a09c0
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-10-07 11:43:58 -0600

    Add Employee model

--- car_rental/api/models.py
+++ car_rental/api/models.py
@@ -9,3 +9,21 @@ class Branch(models.Model):
     streetnumber = models.CharField(max_length=10)
     streetname = models.CharField(max_length=30)
     unitnumber = models.CharField(max_length=5)
+
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    salt = models.CharField(max_length=255)
+    salary = models.IntegerField()
+    rank = models.CharField(max_length=30)
+    DOB = models.DateTimeField()
+    province = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    postal_code = models.CharField(max_length=6)
+    street_number = models.CharField(max_length=10)
+    street_name = models.CharField(max_length=30)
+    unit_number = models.CharField(max_length=5)
+    
