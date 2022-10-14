6cfb763ed89bfa6d51a44ecd9563713e8018adc0
Hieu Nguyen <axelnguyen0701@gmail.com>
2022-10-14 14:53:27 -0600

    Add works at relationship

--- car_rental/api/models.py
+++ car_rental/api/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Branch(models.Model):
     id = models.AutoField(primary_key=True)
     province = models.CharField(max_length=30)
@@ -14,40 +15,44 @@ class Branch(models.Model):
     street_name = models.CharField(max_length=30)
     unit_number = models.CharField(max_length=5)
 
+
 class BranchPhoneNumber(models.Model):
     id = models.AutoField(primary_key=True)
     branch_id = models.ForeignKey('Branch', on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
 
+
 class CarType(models.Model):
-    car_type_id         = models.AutoField(primary_key=True)
-    description         = models.CharField(max_length = 50)
-    daily_cost          = models.FloatField(max_length = 10)
-    weekly_cost         = models.FloatField(max_length = 10)
-    monthly_cost        = models.FloatField(max_length = 10)
-    late_fee            = models.FloatField(max_length = 10)
-    change_branch_fee   = models.FloatField(max_length = 10)
+    car_type_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=50)
+    daily_cost = models.FloatField(max_length=10)
+    weekly_cost = models.FloatField(max_length=10)
+    monthly_cost = models.FloatField(max_length=10)
+    late_fee = models.FloatField(max_length=10)
+    change_branch_fee = models.FloatField(max_length=10)
 
     def __str__(self):
         return self.description
 
-class Car(models.Model):
-    #choices for status
-    STATUS_CHOICES = (('Available', 'Rented'),('Not Available', 'Not Rented'))
 
-    car_id              = models.AutoField(primary_key=True)
-    car_type            = models.ForeignKey(CarType, related_name= 'car_type', on_delete=models.CASCADE)
-    branch              = models.ForeignKey(Branch, related_name= 'car_branch', default= '', on_delete=models.CASCADE)
-    manufacturer        = models.CharField(max_length = 50)
-    model               = models.CharField(max_length = 50)
-    fuel_type           = models.CharField(max_length = 15)
-    colour              = models.CharField(max_length = 15)
-    license_plate       = models.CharField(max_length = 10)
-    status              = models.CharField(max_length = 20, choices = STATUS_CHOICES)
-    mileage             = models.FloatField(max_length = 25)
+class Car(models.Model):
+    # choices for status
+    STATUS_CHOICES = (('Available', 'Rented'), ('Not Available', 'Not Rented'))
+
+    car_id = models.AutoField(primary_key=True)
+    car_type = models.ForeignKey(CarType, related_name='car_type', on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, related_name='car_branch', default='', on_delete=models.CASCADE)
+    manufacturer = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    fuel_type = models.CharField(max_length=15)
+    colour = models.CharField(max_length=15)
+    license_plate = models.CharField(max_length=10)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    mileage = models.FloatField(max_length=25)
 
     def __str__(self):
         return self.manufacturer + self.model
+
 
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
@@ -66,6 +71,7 @@ class Employee(models.Model):
     street_number = models.CharField(max_length=10)
     street_name = models.CharField(max_length=30)
     unit_number = models.CharField(max_length=5)
+    works_at = models.ForeignKey(Branch, models.DO_NOTHING)
 
 
 class EmployeePhoneNumber(models.Model):
@@ -89,16 +95,18 @@ class Customer(models.Model):
     street_name = models.CharField(max_length=30)
     unit_number = models.CharField(max_length=5)
 
+
 class CustomerPhoneNumber(models.Model):
     id = models.AutoField(primary_key=True)
     customer_id = models.ForeignKey(Customer, models.DO_NOTHING)
     phone_number = models.CharField(max_length=15)
 
+
 class Rental(models.Model):
-    rental_id =  models.AutoField(primary_key=True)
+    rental_id = models.AutoField(primary_key=True)
     date_from = models.DateField()
     date_to = models.DateField()
     date_returned = models.DateField()
-    total_cost = models.FloatField(max_length = 10)
+    total_cost = models.FloatField(max_length=10)
     licence_plate = models.ForeignKey(Car, models.DO_NOTHING)
     gold_member = models.ForeignKey(Customer, models.DO_NOTHING)
--- car_rental/api/views.py
+++ car_rental/api/views.py
@@ -6,37 +6,46 @@ from .models import Branch, BranchPhoneNumber, Employee, Customer, CustomerPhone
 
 # Create your views here.
 
+
 class RentalView(viewsets.ModelViewSet):
     serializer_class = RentalSerializer
     queryset = Rental.objects.all()
-    
+
+
 class BranchView(viewsets.ModelViewSet):
     serializer_class = BranchSerializer
     queryset = Branch.objects.all()
+
 
 class BranchPhoneNumberView(viewsets.ModelViewSet):
     serializer_class = BranchPhoneNumberSerializer
     queryset = BranchPhoneNumber.objects.all()
 
+
 class CarTypeView(viewsets.ModelViewSet):
     serializer_class = CarTypeSerializer
     queryset = CarType.objects.all()
+
 
 class CarView(viewsets.ModelViewSet):
     serializer_class = CarSerializer
     queryset = Car.objects.all()
 
+
 class EmployeeView(viewsets.ModelViewSet):
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
+
 
 class EmployeePhoneNumberView(viewsets.ModelViewSet):
     serializer_class = EmployeePhoneNumberSerializer
     queryset = EmployeePhoneNumber.objects.all()
 
+
 class CustomerView(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
+
 
 class CustomerPhoneNumberView(viewsets.ModelViewSet):
     serializer_class = CustomerPhoneNumberSerializer
