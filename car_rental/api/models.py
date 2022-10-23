5db440f6a11c3e8dd5519a56cec0332f38c9539c
Michael Fauht <fauhtm@mymacewan.ca>
2022-10-23 17:17:56 -0600

    re do vlads changes (accidentally undid)

--- car_rental/api/models.py
+++ car_rental/api/models.py
@@ -16,7 +16,7 @@ class Branch(models.Model):
     unit_number = models.CharField(max_length=5, blank=True)
 
     def __str__(self):
-        return "Branch " + str(self.id) 
+         return self.street_number + " " + self.street_name + " " + self.city 
 
 
 class BranchPhoneNumber(models.Model):
@@ -40,7 +40,7 @@ class CarType(models.Model):
 
 class Car(models.Model):
     # choices for status
-    STATUS_CHOICES = (('Available', 'Rented'), ('Not Available', 'Not Rented'))
+    STATUS_CHOICES = (('Available', 'Not Rented'),('Not Available', 'Rented'))
 
     car_id = models.AutoField(primary_key=True)
     car_type = models.ForeignKey(CarType, related_name='car_type', on_delete=models.CASCADE)
@@ -54,7 +54,7 @@ class Car(models.Model):
     mileage = models.FloatField(max_length=25)
 
     def __str__(self):
-        return self.manufacturer + self.model
+        return self.license_plate
 
 
 class Employee(models.Model):
