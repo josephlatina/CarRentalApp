16f68beb92a1fa6d0875a22414ab4f284a51157e
josephlatina <josephlatina93@gmail.com>
2022-11-22 11:03:37 -0700

    completed connection to backend for filter, reserve summary and car cards

--- ReactApp/car_rental_react/src/components/CarCard.js
+++ ReactApp/car_rental_react/src/components/CarCard.js
@@ -1,33 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import '../css/style.css';
 
 const CarCard = (props) => {
+    const [ carType, setCarType ] = useState([]);
+    const [ total, setTotal ] = useState(0);
+
+    const navigate = useNavigate();
+
+    // handle retrieval of car types from query here
+    // const queryCarTypes = async () => {
+    //     // handle query here
+    //     // try {
+    //     //     axios
+    //     //         .get("/api/cartypes")
+    //     //         .then((res) => setCarType(res.data.filter((cartype) => {
+    //     //             return cartype.car_type_id == props.cartypeid;
+    //     //         })))
+    //     //         .catch((err) => console.log(err));
+    //     // } catch (error) {
+    //     //     throw new Error(error);
+    //     // }
+    //     setCarType(props.cartypes.filter((carType) => {
+    //         return cartype.car_type_id == props.cartypeid;
+    //     }))
+    // };
+
+    const handleCalculation = () => {
+        const returnDate = new Date(props.return);
+        const pickupDate = new Date(props.pickup);
+        let estimatedCost = 0;
+
+        if (returnDate >= pickupDate) {
+            const diff = returnDate - pickupDate;
+            const diffDays = diff/(1000 * 60 * 60 * 24);
+
+            if (diffDays < 7) {
+                estimatedCost = diffDays * props.cartypeitem[0]?.daily_cost;
+            }
+            if (30 > diffDays >= 7) {
+                const weeks = diffDays / 7;
+                const leftDays = diffDays - (weeks * 7);
+                estimatedCost = (weeks * props.cartypeitem[0]?.weekly_cost) + (leftDays * props.cartypeitem[0]?.daily_cost);
+            }
+            if (diffDays >= 30) {
+                const months = diffDays / 30;
+                let leftDays = diffDays - (months * 30);
+                let weeks = 0;
+                if (leftDays > 7) {
+                    weeks = leftDays / 7;
+                    leftDays = leftDays - (weeks * 7);
+                }
+                estimatedCost = (months * props.cartypeitem[0]?.monthly_cost) + (weeks * props.cartypeitem[0]?.weekly_cost) + (leftDays * props.cartypeitem[0]?.daily_cost);
+            }
+        }
+
+        setTotal(estimatedCost);
+        
+    }
+
+    const handleSubmit = () => {
+        console.log("hello");
+
+    }
+
+    useEffect(() => {
+        // call on this method to retrieve car types
+        // (async () => {
+        //     await queryCarTypes();
+        // })();
+
+        // console.log("hello");
+        // console.log(props.cartype2);
+        // setCarType(props.cartype2);
+        // console.log(props.cartype2);
+
+        // handleCalculation(props.cartype2.filter((cartype) => {
+        //     return cartype.car_type_id == props.cartypeid;
+        // }));
+        handleCalculation();
+    }, []);
 
     return (
-        <div class="row box" id="car-box">
+        <div className="row box" id="car-box">
             {/* Car Image */}
-            <div class="col-4 car-info">
+            <div className="col-4 car-info">
                 <p>Image here</p>
             </div>
             {/* Car Info */}
-            <div class="col-4 car-info">
-                <h3>Compact</h3>
-                <p>Nissan Versa or similar</p>
-                <p>5</p>
-                <p>2</p>
-                <p>Automatic</p>
-                <p>4 Door</p>
+            <div className="col-4 car-info">
+                <h3>{props.manufacturer} {props.model}</h3>
+                <p>{props.cartypeitem[0]?.description}</p>
+                <p>{props.fueltype}</p>
             </div>
             {/* Car Price */}
-            <div class="col-4 car-info">
-                <div class="car-price">
+            <div className="col-4 car-info">
+                <div className="car-price">
                     <p>CAD</p>
-                    <h3>239.99</h3>
+                    <h3>{total.toFixed(2)}</h3>
                     <p>Total</p>
                 </div>
                 {/* Button */}
-                <Button className="col-10 car-btn" size="lg">Select</Button>
+                <Button className="col-10 car-btn" size="lg" onClick={handleSubmit}>Select</Button>
             </div>
         </div>
 
--- ReactApp/car_rental_react/src/routes/CarSelection.js
+++ ReactApp/car_rental_react/src/routes/CarSelection.js
@@ -1,45 +1,223 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import ReserveSummary from "../components/ReserveSummary";
 import CarCard from "../components/CarCard";
 import axios from "axios";
 import "../css/style.css";
+import { CardSubtitle, DropdownToggle } from "reactstrap";
 
 const CarSelection = () => {
     const location = useLocation();
-    const [cars, setCars] = useState();
-    const manufacturers = ["Nissan", "Toyota", "Honda", "Lexus", "Dodge"];
-    const fueltype = ["Gasoline", "Electric", "Hybrid"];
-    const cartype = ["Economy", "Compact", "SUV", "Van"];
+    const pickupdate = "2022-10-25";
+    const returndate= "2022-10-28";
+    const [cars, setCars] = useState([{
+        car_id: 6,
+        car_type: 3,
+        manufacturer: "Dodge",
+        model: "Caravan",
+        fuel_type: "Gasoline"
+    }]);
+    const [ manufacturers, setManufacturers ] = useState([]);
+    const [ fueltype, setFuelType ] = useState([]);
+    const [ cartype, setCarType ] = useState([]);
+    const [ filters, setFilters ] = useState({
+        cards: [],
+        manufacturers: new Set(),
+        fueltype: new Set(),
+        cartype: new Set(),
+    })
+    // const manufacturers = ["Nissan", "Toyota", "Honda", "Lexus", "Dodge"];
+    // const fueltype = ["Gasoline", "Electric", "Hybrid"];
+    // const cartype = ["Economy", "Compact", "SUV", "Van"];
 
-    useEffect(() => {}, []);
+    // handle retrieval of cars from query here
+    const queryCars = async () => {
+        // parameters here
+
+        // handle query here
+        try {
+            axios
+                .get("/api/cars")
+                .then((res) => {
+                    setFilters({...filters, cards: res.data});
+                    setCars(res.data)})
+                .catch((err) => console.log(err));
+        } catch (error) {
+            throw new Error(error);
+        }
+
+        let car_manufacturers = [];
+        let car_fueltype = [];
+
+        for (let car of cars) {
+            car_manufacturers.push(car.manufacturer);
+            car_fueltype.push(car.fuel_type);
+        }
+        setManufacturers(Array.from(new Set(car_manufacturers)));
+        setFuelType(Array.from(new Set(car_fueltype)));
+    };
+
+    // handle retrieval of car types from query here
+    const queryCarTypes = async () => {
+        // handle query here
+        try {
+            axios
+                .get("/api/cartypes")
+                .then((res) => setCarType(res.data))
+                .catch((err) => console.log(err));
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
+
+    const handleManufacturerFilterChange = useCallback(event => {
+        setFilters(prevFilter => {
+            let manufacturers = new Set(prevFilter.manufacturers)
+            let fueltype = new Set(prevFilter.fueltype)
+            let cartype = new Set(prevFilter.cartype)
+            let cards = cars
+
+            if (event.target.checked) {
+                manufacturers.add(event.target.value)
+            } else {
+                manufacturers.delete(event.target.value)
+            }
+
+            if (manufacturers.size) {
+                cards = cards.filter(card => {
+                    return manufacturers.has(card.manufacturer)
+                })
+            }
+            if (fueltype.size) {
+                cards = cards.filter(card => {
+                    return fueltype.has(card.fuel_type)
+                })
+            }
+            if (cartype.size) {
+                cards = cards.filter(card => {
+                    return cartype.has(card.car_type.toString())
+                })
+            }
+
+            return {
+                manufacturers,
+                fueltype,
+                cartype,
+                cards
+            }
+        })
+    }, [setFilters])
+
+    const handleFuelTypeFilterChange = useCallback(event => {
+        setFilters(prevFilter => {
+            let manufacturers = new Set(prevFilter.manufacturers)
+            let fueltype = new Set(prevFilter.fueltype)
+            let cartype = new Set(prevFilter.cartype)
+            let cards = cars
+
+            if (event.target.checked) {
+                fueltype.add(event.target.value)
+            } else {
+                fueltype.delete(event.target.value)
+            }
+
+            if (fueltype.size) {
+                cards = cards.filter(card => {
+                    return fueltype.has(card.fuel_type)
+                })
+            }
+            if (cartype.size) {
+                cards = cards.filter(card => {
+                    return cartype.has(card.car_type.toString())
+                })
+            }
+            if (manufacturers.size) {
+                cards = cards.filter(card => {
+                    return manufacturers.has(card.manufacturer)
+                })
+            }
+
+            return {
+                manufacturers,
+                fueltype,
+                cartype,
+                cards
+            }
+        })
+    }, [setFilters])
+
+    const handleCarTypeFilterChange = useCallback(event => {
+        setFilters(prevFilter => {
+            let manufacturers = new Set(prevFilter.manufacturers)
+            let fueltype = new Set(prevFilter.fueltype)
+            let cartype = new Set(prevFilter.cartype)
+            let cards = cars
+
+            if (event.target.checked) {
+                cartype.add(event.target.value)
+            } else {
+                cartype.delete(event.target.value)
+            }
+
+            if (cartype.size) {
+                cards = cards.filter(card => {
+                    return cartype.has(card.car_type.toString())
+                })
+            }
+            if (manufacturers.size) {
+                cards = cards.filter(card => {
+                    return manufacturers.has(card.manufacturer)
+                })
+            }
+            if (fueltype.size) {
+                cards = cards.filter(card => {
+                    return fueltype.has(card.fuel_type)
+                })
+            }
+
+            return {
+                manufacturers,
+                fueltype,
+                cartype,
+                cards
+            }
+        })
+    }, [setFilters])
+
+    useEffect(() => {
+        // call on this method to retrieve cars
+        // queryCars();
+        // queryCarTypes();
+        (async () => {
+            await queryCars();
+        })();
+        (async () => {
+            await queryCarTypes();
+        })();
+    }, []);
 
     return (
         <>
             {/* Section 2: Progress */}
-            <section class="container">
+            <section className="container">
                 {/* Progress Bar */}
-                <div class="row" id="progress-bar">
-                    <div class="col-3 bottom-line">
+                <div className="row" id="progress-bar">
+                    <div className="col-4 bottom-line">
                         <h5>1. Rental Details</h5>
                     </div>
-                    <div class="col-3 bottom-line">
-                        <h5>2. Select Branch</h5>
-                    </div>
                     <div
-                        class="col-3 bottom-line"
+                        className="col-4 bottom-line"
                         id="selected"
-                        style={{ "border-bottom": "10px solid #0FB877" }}
                     >
-                        <h5>3. Select Car</h5>
+                        <h5>2. Select Car</h5>
                     </div>
-                    <div class="col-3 bottom-line">
-                        <h5>4. Reserve</h5>
+                    <div className="col-4 bottom-line">
+                        <h5>3. Reserve</h5>
                     </div>
                 </div>
             </section>
             {/* Section 3: Reserve Summary */}
-            <section class="container">
+            <section className="container">
                 <ReserveSummary
                     pickuplocation="Edmonton, AB"
                     returnlocation="Calgary, AB"
@@ -53,23 +231,56 @@ const CarSelection = () => {
                 <div id="filter-box">
                     <div>
                         <h4>Filter By</h4>
+                        <br/>
                         <h5 id="selected">Manufacturer</h5>
-                        {manufacturers.map((item, index) => {
-                            return <p key={index}>{item}</p>;
+                        {manufacturers?.map((item, index) => {
+                            return <div key={index}>
+                                <label>
+                                    <input key={index} type="checkbox" value={item} onChange={handleManufacturerFilterChange}></input>
+                                    &nbsp;&nbsp;{item}
+                                </label>
+                            </div>;
+                            // return <p key={index}>{item}</p>;
                         })}
+                        <br/>
                         <h5 id="selected">Fuel Type</h5>
-                        {fueltype.map((item, index) => {
-                            return <p key={index}>{item}</p>;
+                        {fueltype?.map((item, index) => {
+                            return <div key={index}>
+                                <label>
+                                    <input key={index} type="checkbox" value={item} onChange={handleFuelTypeFilterChange}></input>
+                                    &nbsp;&nbsp;{item}
+                                </label>
+                            </div>;
                         })}
+                        <br/>
                         <h5 id="selected">Car Type</h5>
-                        {cartype.map((item, index) => {
-                            return <p key={index}>{item}</p>;
-                        })}
+                        {cartype?.map((item, index) => {
+                            return <div key={index}>
+                                <label>
+                                    <input key={index} type="checkbox" value={item.car_type_id} onChange={handleCarTypeFilterChange}></input>
+                                    &nbsp;&nbsp;{item.description}
+                                </label>
+                            </div>;
+                            })}
                     </div>
                 </div>
                 {/* Car Cards #393939*/}
-                <div class="box" style={{ "background-color": "#393939" }}>
-                    <CarCard />
+                <div>
+                    {filters.cards.map((item, index) => {
+                    return <div className="box">
+                        <CarCard
+                            key={index}
+                            manufacturer={item.manufacturer}
+                            model={item.model}
+                            fueltype={item.fuel_type}
+                            cartypeitem={cartype?.filter((cartype) => {
+                                return cartype?.car_type_id == item.car_type;
+                            })}
+                            pickup={pickupdate}
+                            return={returndate}
+                        />
+                    </div>;
+                    })}
                 </div>
             </section>
         </>
